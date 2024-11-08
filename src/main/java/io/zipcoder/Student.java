d7169ee22ae909c158928f658fa01427af6054fe
Kayan Reid <kreid@Zipcoders-MacBook-Pro.local>
2024-11-08 11:46:15 -0500

    Defined instance variables for the Student class.

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,12 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    public String firstName;
+    public String lastName;
+    public ArrayList<Double> examScores;
+
+
+
 }
